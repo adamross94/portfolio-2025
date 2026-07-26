c410cb926c3fc0ded1fb4cb0905d84e6ff9f0d77
Adam Ross <66434612+adamross94@users.noreply.github.com>
2026-07-26 13:16:50 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/portfolio-2025/',
   plugins: [tailwindcss(), react()],
 });
